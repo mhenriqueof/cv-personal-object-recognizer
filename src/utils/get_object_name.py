466ee91a5bd3240fd1d8af529fcd93ce4f6f0bdd
Marcio Henrique <mhenriqueof@outlook.com>
2026-01-22 14:48:12 -0300

    feat(utils): implement get_object_name

---
+++ src/utils/get_object_name.py
@@ -0,0 +1,7 @@
+def get_object_name() -> str:
+    while True:
+        object_name = input("Object name: ").strip()
+        if len(object_name) != 0:
+            return object_name
+        print("Type a valid name.")
+        
